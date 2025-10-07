d50a0ee0992fa24fbd34a8125fef70d13733c663
KtorZ <matthias.benkort@gmail.com>
2025-10-07 16:45:17 +0200

    adjustments and extra comments following review feedback.

--- rust/crates/cardano-tx-builder/src/cardano/macros.rs
+++ rust/crates/cardano-tx-builder/src/cardano/macros.rs
@@ -1,23 +1,3 @@
-//  This Source Code Form is subject to the terms of the Mozilla Public
-//  License, v. 2.0. If a copy of the MPL was not distributed with this
-//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-pub mod address;
-pub mod credential;
-pub mod execution_units;
-pub mod hash;
-pub mod input;
-pub mod network_id;
-pub mod output;
-pub mod plutus_data;
-pub mod plutus_script;
-pub mod plutus_version;
-pub mod protocol_parameters;
-pub mod redeemer_pointer;
-pub mod resolved_input;
-pub mod transaction;
-pub mod value;
-
 #[macro_export]
 macro_rules! input {
     ($tx_hex:expr, $index:expr $(,)?) => {
