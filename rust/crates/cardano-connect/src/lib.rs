67f5e8249ec94fe2b6908d12eb3a1a3f9d2898e2
KtorZ <matthias.benkort@gmail.com>
2026-02-14 00:58:17 +0100

    Fix wasm export of Network
      Turns out we cannot attach methods to enums where all variants are nullary constructors with no fields; this creates a namespace conflict on the js side. So we have to resort to defining separate standalone functions.

--- rust/crates/cardano-connect/src/lib.rs
+++ rust/crates/cardano-connect/src/lib.rs
@@ -1,5 +1,4 @@
-pub use crate::cardano_connect::CardanoConnect;
-pub use crate::network::Network;
+pub use crate::{cardano_connect::CardanoConnect, network::Network};
 
 mod cardano_connect;
 mod network;
